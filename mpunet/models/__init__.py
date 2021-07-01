b64834a3c8b8060d6d3ae3ace2fa0fd62cbfd801
sandeepsinghsengar <46027368+sandeepsinghsengar@users.noreply.github.com>
2021-07-01 12:16:56 +0200

    Update __init__.py

--- mpunet/models/__init__.py
+++ mpunet/models/__init__.py
@@ -5,6 +5,7 @@ from .model_init import model_initializer
 from .multitask_unet2d import MultiTaskUNet2D
 from .unet3plus import UNet3Plus
 from .unet2plus import UNet2Plus
+from .unet2plus_deep import UNet2Plus_deep
 from .unet2plus1 import UNet2Plus1
 from .unet2plus2 import UNet2Plus2
 from .unet2plus_up import UNet2Plus_up
@@ -20,6 +21,7 @@ PREPARATION_FUNCS = {
     "UNet3Plus_deep": dpf.prepare_for_multi_view_unet,
     "UNet3D": dpf.prepare_for_3d_unet,
     "UNet2Plus": dpf.prepare_for_multi_view_unet,
+    "UNet2Plus_deep": dpf.prepare_for_multi_view_unet,
     "UNet2Plus_up": dpf.prepare_for_multi_view_unet,
     "UNet2Plus_up_deep": dpf.prepare_for_multi_view_unet,
     "UNet2Plus1": dpf.prepare_for_multi_view_unet,
